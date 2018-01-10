912c5bd3d056b2018bcedfd0d022d344b783baab
Ankita Luthra <ankital@thoughtworks.com>
2018-01-10 10:52:11 +0530

    Add routes - Shirish/Ankita

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,8 +1,23 @@
 import * as React from 'react';
+import {Route} from "react-router-dom";
+
+const Header = () => <div><h1>Header</h1></div>;
+const Requests = () => <div><h1>Requests</h1></div>;
+const Dashboard = () => <div><h1>Dashboard</h1></div>;
+const Body = (props) => <div>{props.children}</div>;
+const Footer = () => <div><h1>Footer</h1></div>;
 
 export default class Layout extends React.Component {
-
   render() {
-    return (<div>Test</div>);
+    return (
+      <div id="app">
+        <Header />
+        <Body>
+          <Route exact path="/" component={Dashboard} />
+          <Route exact path="/requests" component={Requests} />
+        </Body>
+        <Footer />
+      </div>
+    );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import './index.scss';
-import App from './App';
+import Layout from './components/Layout/Layout';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Route component={Layout}/>
   </BrowserRouter>,
   document.getElementById('root'),
 );
