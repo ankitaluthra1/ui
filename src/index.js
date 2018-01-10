52e23af2d136fa24a2b7e0c3dcd4e3731e49e75c
Ankita Luthra <ankital@thoughtworks.com>
2018-01-10 12:44:34 +0530

    Fix css import issue Shirish/Ankita

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Route} from 'react-router-dom';
-import './index.scss';
 import Layout from './components/Layout/Layout';
+import './index.css';
 
 ReactDOM.render(
   <BrowserRouter>
