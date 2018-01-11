667eebee1394941b9b0d3acb5e96337fe2eee723
Navin Singh <navinks@thoughtworks.com>
2018-01-11 15:54:12 +0530

    Move fonts and stylesheets to styles directory Navin/Shabareesh

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter, Route} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import Layout from './components/Layout/Layout';
-import './index.css';
+import './styles/index.css';
 import store from './store';
 
 ReactDOM.render(
