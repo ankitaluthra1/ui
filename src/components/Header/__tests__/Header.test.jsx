8c4b1c5e48c5cbbae385de80442f70db99d4c838
Navin Singh <navinks@thoughtworks.com>
2018-01-12 15:01:15 +0530

    Add test for Header component - Navin/Shirish

--- src/components/Header/__tests__/Header.test.jsx
+++ src/components/Header/__tests__/Header.test.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import Header from '../Header';
+import Logo from '../Logo/Logo';
+import Navigation from '../Navigation/Navigation';
+import AccountControl from '../AccountControl/AccountControl';
 
 describe('Header Component', () => {
-  it('should have a header', function () {
-    const component = shallow(<Header/>);
+  const component = shallow(<Header/>);
+  it('should have a header', () => {
     expect(component.find('.header')).toHaveLength(1);
+  });
+
+  it('should have Logo', () => {
+    expect(component.find(Logo)).toHaveLength(1);
+  });
+
+  it('should have Navigation', () => {
+    expect(component.find(Navigation)).toHaveLength(1);
+  });
+
+  it('should have AccountControl', () => {
+    expect(component.find(AccountControl)).toHaveLength(1);
   });
 });
